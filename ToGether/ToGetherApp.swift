22e858c0f6cee3b46f599b21d4a28fcfde62a650
YujanTing <yu-jan.ting@emory.edu>
2021-10-04 19:12:03 -0400

    Embed event pictures in Zbox.

---
+++ ToGether/ToGetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToGetherApp.swift
+//  ToGether
+//
+//  Created by Yujan Ting on 2021/10/4.
+//
+
+import SwiftUI
+
+@main
+struct ToGetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
